1855c68dc08f6e3bad390752bc03e3e813fdc271
Paulo Moreira <paulomoreiradatascientist@gmail.com>
2025-08-25 20:52:25 -0300

    Update Python Bootcamp

---
+++ Projetos/Aula09-Logger-Decorator/exemplo00.py
@@ -0,0 +1,7 @@
+from loguru import logger
+
+logger.debug("um aviso para o desenvolvedor no futuro")
+logger.info("Informação importante do processo")
+logger.warning("Um aviso que algo vai para de funcionar no futuro")
+logger.error("Aconteceu uma falha")
+logger.critical("Aconteceu uma situação que abortou a aplicação")
